5825f30b3f836b831e77860acff4d1fa297b043f
harshxraj <harshraj0588@gmail.com>
2024-03-23 01:29:49 +0530

    deployment test

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { v2 as cloudinary } from "cloudinary";
 import "dotenv/config";
+import path from "path";
+
 import authRouter from "./routes/auth.routes.js";
 import bodyParser from "body-parser";
 import connection from "./db/connection.js";
@@ -12,6 +14,7 @@ import settingRouter from "./routes/settings.routes.js";
 
 const server = express();
 const PORT = 3000;
+const __dirname = path.resolve();
 
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
@@ -28,6 +31,12 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+server.use(express.static(path.join(__dirname, "frontend/dist")));
+
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
 });
 
 server.listen(PORT, () => {
