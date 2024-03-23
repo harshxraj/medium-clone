cc6f6b902e4158e4e7a1299af5c8395aa0d2549e
harshxraj <harshraj0588@gmail.com>
2024-03-24 01:27:05 +0530

    test9

---
+++ backend/cron.js
@@ -0,0 +1,20 @@
+import cron from "cron";
+import https from "https";
+
+const URL = "https://chatapp-zdyr.onrender.com";
+
+const job = new cron.CronJob("*/14 * * * *", function () {
+  https
+    .get(URL, (res) => {
+      if (res.statusCode === 200) {
+        console.log("GET request sent successfully");
+      } else {
+        console.log("GET request failed", res.statusCode);
+      }
+    })
+    .on("error", (e) => {
+      console.error("Error while sending request", e);
+    });
+});
+
+export default job;
