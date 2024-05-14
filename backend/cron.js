20526d95ccd75373d91d0f6d04e1ef245fdb126f
Harsh Raj <128404446+harshxraj@users.noreply.github.com>
2024-05-14 09:37:12 +0530

    Update cron.js

--- backend/cron.js
+++ backend/cron.js
@@ -17,6 +17,20 @@ const URL = "https://medium-ix5b.onrender.com";
 //     });
 // });
 
+// const job = new cron.CronJob("*/14 9-22 * * *", function () {
+//   https
+//     .get(URL, (res) => {
+//       if (res.statusCode === 200) {
+//         console.log("GET request sent successfully");
+//       } else {
+//         console.log("GET request failed", res.statusCode);
+//       }
+//     })
+//     .on("error", (e) => {
+//       console.error("Error while sending request", e);
+//     });
+// });
+
 const job = new cron.CronJob("*/14 9-22 * * *", function () {
   https
     .get(URL, (res) => {
@@ -29,7 +43,7 @@ const job = new cron.CronJob("*/14 9-22 * * *", function () {
     .on("error", (e) => {
       console.error("Error while sending request", e);
     });
-});
+}, null, true, 'Asia/Kolkata');
 
 
 export default job;
